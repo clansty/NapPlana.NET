2b9d3d0a40ce7e2fb504e50cba7c32fd94748379
凌莞~(=^▽^=) <opensource@c5y.moe>
2026-02-21 14:28:48 +0800

    feat: 合并转发相关功能 (#3)
    * feat: 合并转发相关功能
    
    * chore: 修改部分注释

--- NapPlana.Net.Core/Data/Message/NodeMessage.cs
+++ NapPlana.Net.Core/Data/Message/NodeMessage.cs
@@ -1,4 +1,6 @@
-﻿using System.Text.Json.Serialization;
+using System.Text.Json.Serialization;
+using NapPlana.Core.Data.API;
+using NapPlana.Core.Utilities;
 
 namespace NapPlana.Core.Data.Message;
 
@@ -22,7 +24,24 @@ public class NodeMessageData : MessageDataBase
     /// <summary>
     /// 内容。
     /// </summary>
-    [JsonPropertyName("content")] public List<MessageDataBase>? Content { get; set; }
+    [JsonConverter(typeof(MessageListConverter))]
+    [JsonPropertyName("content")] public List<MessageBase>? Content { get; set; }
+    /// <summary>
+    /// 转发卡片标题行的内容（xxx 的聊天记录）（嵌套转发时使用）。
+    /// </summary>
+    [JsonPropertyName("source")] public string? Source { get; set; }
+    /// <summary>
+    /// 转发卡片预览文本列表（嵌套转发时使用）。
+    /// </summary>
+    [JsonPropertyName("news")] public List<ForwardNewsItem>? News { get; set; }
+    /// <summary>
+    /// 转发卡片摘要（点击查看 x 条聊天记录）（嵌套转发时使用）。
+    /// </summary>
+    [JsonPropertyName("summary")] public string? Summary { get; set; }
+    /// <summary>
+    /// 转发卡片在消息列表中的外显文本（[聊天记录]）（嵌套转发时使用）。
+    /// </summary>
+    [JsonPropertyName("prompt")] public string? Prompt { get; set; }
 }
 
 /// <summary>
